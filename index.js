2d450ff64c62fa478ba9c4567c8206544f13b346
Alejandro Rayas <alexrayas@rocketmail.com>
2019-10-30 11:34:47 -0700

    Update index.js

--- index.js
+++ index.js
@@ -6,66 +6,78 @@ for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
 
         var buttonInnerHTML = this.innerHTML;
 
-        switch (buttonInnerHTML) {
-            case "w":
-                var audio = new Audio("sounds/crash.mp3");
-                audio.play();
-
-            break;
-
-            case "a":
-                var audio = new Audio("sounds/kick-bass.mp3");
-                audio.play();
-
-            break;
-
-            case "s":
-                var audio = new Audio("sounds/snare.mp3");
-                audio.play();
-
-            break;
-
-            case "d":
-                var audio = new Audio("sounds/tom-1.mp3");
-                audio.play();
-
-            break;
-
-            case "j":
-                var audio = new Audio("sounds/tom-2.mp3");
-                audio.play();
-
-            break;
-
-            case "k":
-                var audio = new Audio("sounds/tom-3.mp3");
-                audio.play();
-
-            break;
-
-            case "l":
-                var audio = new Audio("sounds/tom-4.mp3");
-                audio.play();
-
-            break;
-
-            default:
-                break;
-        }
+        makeSoundSwitch(buttonInnerHTML);    
 
     });
 
 
 }
 
+document.addEventListener("keypress", function(event) {
 
-function handleClick() {
-    var audio = new Audio("sounds/tom-1.mp3");
-    audio.play();
+    console.log(event.key);
+    makeSoundSwitch(event.key);  
+
+})
+
+function makeSoundSwitch(a) {
+    switch (a) {
+        case "w":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+
+        break;
+
+        case "a":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+
+        break;
+
+        case "s":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+
+        break;
+
+        case "d":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+
+        break;
+
+        case "j":
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+
+        break;
+
+        case "k":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+
+        break;
+
+        case "l":
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+
+        break;
+
+        default:
+            console.log();
+
+        break;
+    }
 }
 
-function HouseKeeper(yearsofExperience, name, cleanRepo) {
-    this.yearsofExperience = yearsofExperience;
-    this.name = name;
-    this.cleanRepo = cleanRepo;
-}
+// practice on making a constructor in javascript and assigning methods to it as well
+// function HouseKeeper(yearsofExperience, name, cleanRepo) {
+//     this.yearsofExperience = yearsofExperience;
+//     this.name = name;
+//     this.cleanRepo = cleanRepo;
+//     this.clean = function () {
+//         alert("Cleaning in progress...");
+
+//     }
+// }
